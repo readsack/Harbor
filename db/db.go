1e38a20fb5e85ea8e5e9e070434d9303a3dcaa81
readsack <agarwala.keshav10@gmail.com>
2026-03-04 13:32:50 +0530

    DB Setup Complete

--- db/db.go
+++ db/db.go
@@ -3,21 +3,73 @@ package db
 import (
 	"log"
 	"database/sql"
+	_ "context"
 )
 
 import _ "github.com/ncruces/go-sqlite3/driver"
 import _ "github.com/ncruces/go-sqlite3/embed"
 
-var sqlDB *sql.DB
-
+var AppDB *sql.DB
 func InitDB(){
-	sqlDB, err := sql.Open("sqlite3", "file:app.db")
+	var err error
+	AppDB, err = sql.Open("sqlite3", "file:app.db")
 	if(err != nil) {
 		log.Fatal(err)
 	} 
-	_, err = sqlDB.Exec(`CREATE TABLE users (id INT, name VARCHAR(10))`)
+	
+}
+
+func SetupDB(){
+	
+	tx, err := AppDB.Begin()
 	if err != nil {
 		log.Fatal(err)
+	} else {
+		_, err = tx.Exec(`CREATE TABLE IF NOT EXISTS users (
+							id INTEGER PRIMARY KEY,
+							username VARCHAR(100) NOT NULL,
+							email VARCHAR(200) UNIQUE NOT NULL, 
+							pass VARCHAR(200)
+						)`)
+		if err != nil {
+			_ = tx.Rollback()
+			log.Fatal(err)
+		}
+		_, err = tx.Exec(`CREATE TABLE IF NOT EXISTS orgs (
+								id INTEGER PRIMARY KEY,
+								name VARCHAR(100) NOT NULL,
+								ceo_id INTEGER NOT NULL,
+								FOREIGN KEY (ceo_id) REFERENCES users(id) ON DELETE CASCADE
+							)`)
+		if err != nil {
+			_ = tx.Rollback()
+			log.Fatal(err)
+		}
+		_, err = tx.Exec(`CREATE TABLE IF NOT EXISTS teams (
+								id INTEGER PRIMARY KEY,
+								name VARCHAR(100) NOT NULL,
+								org_id INTEGER NOT NULL,
+								FOREIGN KEY (org_id) REFERENCES orgs(id) ON DELETE CASCADE
+							)`)
+		if err != nil {
+			_ = tx.Rollback()
+			log.Fatal(err)
+		}
+		_, err = tx.Exec(`CREATE TABLE IF NOT EXISTS user_team (
+								user_id INTEGER NOT NULL,
+								team_id INTEGER NOT NULL,
+								user_role INTEGER NOT NULL,
+								FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+								FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE
+							)`)
+		if err != nil {
+			_ = tx.Rollback()
+			log.Fatal(err)
+		}
 	}
+	if err := tx.Commit(); err != nil {
+		log.Fatal(err)
+	}
+	
 }
 
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ const PORT string = ":8080"
 func main(){
 	log.Println("Server starting At localhost"+PORT)
 	db.InitDB()
+	db.SetupDB()
 	log.Fatal(http.ListenAndServe(PORT, nil))
 	
 }
