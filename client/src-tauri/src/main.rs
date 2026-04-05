8a6950c0ed872103d80c8915e8768b5883f0a0c9
readsack <agarwala.keshav10@gmail.com>
2026-04-06 05:12:37 +0530

    Switched to Svelte

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    harbor_lib::run()
+    harbor_app_lib::run()
 }
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,8 +1,48 @@
-/*
+<script>
+  import { invoke } from "@tauri-apps/api/core";
 
-.logo.vanilla:hover {
-  filter: drop-shadow(0 0 2em #ffe21c);
+  let name = $state("");
+  let greetMsg = $state("");
+
+  async function greet(event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<main class="container">
+  <h1>Welcome to Tauri + Svelte</h1>
+
+  <div class="row">
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+    </a>
+    <a href="https://tauri.app" target="_blank">
+      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank">
+      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
+    </a>
+  </div>
+  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
+
+  <form class="row" onsubmit={greet}>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button type="submit">Greet</button>
+  </form>
+  <p>{greetMsg}</p>
+</main>
+
+<style>
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
 }
+
+.logo.svelte-kit:hover {
+  filter: drop-shadow(0 0 2em #ff3e00);
+}
+
 :root {
   font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
   font-size: 16px;
@@ -113,5 +153,4 @@ button {
   }
 }
 
-*/
-
+</style>
